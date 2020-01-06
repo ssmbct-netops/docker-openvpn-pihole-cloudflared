4f32556073b6d9eb82a9911ab9d8301a229ebd12
Dennis van der Wals <dennis@vdwals.de>
2020-01-06 12:00:49 +0100

    added remote repos

--- init.sh
+++ init.sh
@@ -2,3 +2,12 @@
 
 echo "Get source repositories"
 apt-get update && apt-get install -y git
+
+git clone https://github.com/mr-bolle/docker-openvpn-pihole.git
+
+cp docker-openvpn-pihole/openvpn-client.sh .
+cp docker-openvpn-pihole/openvpn-install.sh .
+
+chmod +x openvpn-client.sh
+chmod +x openvpn-install.sh
+./openvpn-install.sh
